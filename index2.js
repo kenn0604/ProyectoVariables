3a124111e999201818cb1374ec1a9e0c34d633cb
KENY <KENY@TELLO.com>
2025-05-16 01:54:06 -0600

    [ADD], se añadieron las instrucciones para el ejercicio

---
+++ index2.js
@@ -0,0 +1,3 @@
+// Realizar una variable tipo Array que tenga 3 objetos
+// El inge quiere saber las siguientes caracteristicas; nombre, edad, correo, activo y 3 habilidades(tienen que ser un array tambien)
+// y eso es un objeto
