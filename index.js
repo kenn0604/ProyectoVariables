eb3e641a82ec63a8d926cd7ff1444ca3fd22a3c0
KENY <KENY@TELLO.com>
2025-05-16 01:11:01 -0600

    [ADD], se añadio la variable de tipo texto

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ console.log("Tipo number");
 let numero;
 numero = 42;
 console.log("Este es el valor de numero:",numero);
+
+console.log("Tipo string");
+let texto;
+texto = "soy Keny";
+console.log(texto);
